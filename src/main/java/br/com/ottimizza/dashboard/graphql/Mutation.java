bc9fbf3ba3ad5d91c6b413932ed5733e0c06cf68
DiogoMS <dmazui@gmail.com>
2019-07-05 10:31:19 -0300

    gerando mutattions

--- src/main/java/br/com/ottimizza/dashboard/graphql/Mutation.java
+++ src/main/java/br/com/ottimizza/dashboard/graphql/Mutation.java
@@ -1,30 +1,45 @@
-package br.com.ottimizza.dashboard.graphql.mutations;
+package br.com.ottimizza.dashboard.graphql;
+
+import java.math.BigInteger;
 
 import javax.persistence.EntityManager;
 
 import br.com.ottimizza.dashboard.models.Company;
+import br.com.ottimizza.dashboard.models.Kpi;
 import br.com.ottimizza.dashboard.models.QCompany;
+import br.com.ottimizza.dashboard.models.QKpi;
 import br.com.ottimizza.dashboard.repositories.company.CompanyRepository;
+import br.com.ottimizza.dashboard.repositories.kpi.KpiRepository;
 import io.leangen.graphql.annotations.GraphQLMutation;
 
-public class CompanyMutation {
+public class Mutation {
 
 	private QCompany company = QCompany.company;
 	private EntityManager em;
 	private CompanyRepository companyRepository;
-	
-	public CompanyMutation(EntityManager em, CompanyRepository companyRepository) {
+
+	private QKpi kpi = QKpi.kpi;
+	private KpiRepository kpiRepository;
+
+	public Mutation(EntityManager em, CompanyRepository companyRepository) {
 		this.em = em;
 		this.companyRepository = companyRepository;
 	}
 
 	@GraphQLMutation //1
-//	public Company createCompany(String cnpj, String name, @GraphQLRootContext AuthorizationContext context) { //2
 	public Company createCompany(Company filter) { //2
 	    Company newCompany = new Company(filter.getCnpj(), filter.getName());
 	    
 	    companyRepository.save(newCompany);
 	    return newCompany;
 	}
-}
+	
+	@GraphQLMutation(name = "editKpi")
+	public Kpi editKpi(BigInteger id, String title) {
+		Kpi kpi = kpiRepository.findById(id);
+		kpi.setTitle(title);
+		
+		return kpiRepository.save(kpi);
+	}
 
+}
