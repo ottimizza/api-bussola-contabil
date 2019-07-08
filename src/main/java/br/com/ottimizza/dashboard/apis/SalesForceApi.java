e212f262749fcc66d4b06dfab721d8278047f55e
DiogoMS <dmazui@gmail.com>
2019-07-08 11:39:06 -0300

    ajuste api SF

--- src/main/java/br/com/ottimizza/dashboard/apis/SalesForceApi.java
+++ src/main/java/br/com/ottimizza/dashboard/apis/SalesForceApi.java
@@ -6,7 +6,12 @@ import com.sforce.soap.partner.QueryResult;
 import com.sforce.soap.partner.sobject.SObject;
 import com.sforce.ws.ConnectionException;
 import com.sforce.ws.ConnectorConfig;
+
+import javassist.expr.NewArray;
+
 import java.util.ArrayList;
+
+import org.json.JSONObject;
 
 public class SalesForceApi {
     
@@ -21,30 +26,35 @@ public class SalesForceApi {
     }
 
     //<editor-fold defaultstate="collapsed" desc="Busca de lista CNPJ por email">
-    public ArrayList<String> getCNPJbyEmail(String emailDono){
-        ArrayList<String> cpfCnpj = new ArrayList<>();
+    public ArrayList<JSONObject> getCNPJbyEmail(String emailDono){
+        ArrayList<JSONObject> empresas = new ArrayList<>();
+        
         try {
             // Instancia conexão com Salesforce. 
             PartnerConnection sfConnection = new PartnerConnection(config());
             
             // Obtem o Roteiro /////////////////////////////////////////////////
             StringBuilder sql = new StringBuilder();
-            sql.append("SELECT CNPJ__c FROM Empresa__c WHERE Email_Dono_Empresa__c='").append(emailDono).append("'");
+            sql.append("SELECT CNPJ__c, name FROM Empresa__c WHERE Email_Dono_Empresa__c like '%").append(emailDono).append("%'");
 
             //String cnpjResultado = "";
             QueryResult qres1 = sfConnection.query(sql.toString());
             if (qres1.getSize() > 0) {
                 for (int xx=0;xx<qres1.getSize();xx++) {
-                    SObject empresas = qres1.getRecords()[xx];
-                    Object campoOb = empresas.getField("CNPJ__c");
-                    if (campoOb != null) cpfCnpj.add(campoOb.toString());  // retornar todos os CNPJs para o Bussola
+
+                    SObject resultados = qres1.getRecords()[xx];
+                    JSONObject campoOb = new JSONObject();
+                    campoOb.put("cnpj", resultados.getField("CNPJ__c"));
+                    campoOb.put("name", resultados.getField("name"));
+                    
+                    if (campoOb != null) empresas.add(campoOb);
                 }
             }
         } catch (ConnectionException e) {
             e.printStackTrace();
         }
 
-        return cpfCnpj;
+        return empresas;
     }
     //</editor-fold>
     
