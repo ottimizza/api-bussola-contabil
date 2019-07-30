1db14f39dc1ca760a9a5bfc01c6bf719dd6e9348
DiogoMS <dmazui@gmail.com>
2019-07-30 14:23:23 -0300

    ajustes em createdetails kpiid createkpidetailcreate

--- src/main/java/br/com/ottimizza/dashboard/controllers/KpiDetailController.java
+++ src/main/java/br/com/ottimizza/dashboard/controllers/KpiDetailController.java
@@ -3,7 +3,10 @@ package br.com.ottimizza.dashboard.controllers;
 import br.com.ottimizza.dashboard.models.Kpi;
 import br.com.ottimizza.dashboard.models.KpiDetail;
 import br.com.ottimizza.dashboard.models.KpiDetailCreate;
+import br.com.ottimizza.dashboard.repositories.kpi.KpiRepository;
 import br.com.ottimizza.dashboard.services.KpiDetailService;
+import br.com.ottimizza.dashboard.services.KpiService;
+import javassist.expr.NewArray;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
@@ -28,41 +31,50 @@ import org.springframework.web.bind.annotation.RestController;
 public class KpiDetailController {
 
 	@Inject
-	KpiDetailService kpiService;
+	KpiDetailService detailService;
+
+	@Inject
+	KpiService kpiService;
 
 	@PostMapping("save")
 	public ResponseEntity<KpiDetail> saveKpi(@RequestBody KpiDetail kpi) throws Exception {
-		return ResponseEntity.ok(kpiService.save(kpi));
+		return ResponseEntity.ok(detailService.save(kpi));
 	}
 
 	@GetMapping("find/{id}")
 	public ResponseEntity<Optional<KpiDetail>> findKpiByID(@PathVariable("id") BigInteger idKpi) throws Exception {
-		return ResponseEntity.ok(kpiService.findById(idKpi));
+		return ResponseEntity.ok(detailService.findById(idKpi));
 	}
 
 	@RequestMapping(value = "/find/cnpj", method = RequestMethod.POST, consumes = "application/json")
 	public ResponseEntity<List<KpiDetail>> findKpiDetailByCNPJ(@RequestBody Map<String, List<String>> body)
 			throws Exception {
 		List<String> listaCNPJ = body.get("cnpj");
-		return ResponseEntity.ok(kpiService.findByListCNPJ(listaCNPJ));
+		return ResponseEntity.ok(detailService.findByListCNPJ(listaCNPJ));
 	}
 
 	@DeleteMapping("delete/{id}")
 	public ResponseEntity<String> removeKpi(@PathVariable("id") BigInteger idKpi) throws Exception {
-		return ResponseEntity.ok(kpiService.delete(idKpi).toString());
+		return ResponseEntity.ok(detailService.delete(idKpi).toString());
 	}
 
 	@PostMapping("createDetails")
-//	public ResponseEntity<List<KpiDetail>> createDetails(@RequestBody Map<String, List<KpiDetail>> body) throws Exception {
 	public ResponseEntity<List<KpiDetail>> createDetails(@RequestBody KpiDetailCreate body) throws Exception {
+		List<KpiDetail> listReturn = new ArrayList<KpiDetail>();		
+		List<KpiDetail> listDetails = new ArrayList<KpiDetail>();		
+		BigInteger idKpi = body.getIdKpi();
+		Kpi kpi = new Kpi();
 		
+		Optional<Kpi> optionalKpi = kpiService.findById(idKpi);
+		try {kpi = optionalKpi.get();}
+		catch(Exception ee) {}
 		
-		System.out.println(">>> 0 "+ body.getIdKpi());
-		List<KpiDetail> listaRet = new ArrayList<KpiDetail>();		
-		
-		System.out.println(">>> 1 "+ body.getKpisDetail().size()); 
-
-		return ResponseEntity.ok(listaRet);
+		for (KpiDetail kpiDetail : listDetails) {
+			KpiDetail detail = new KpiDetail();
+			detail = kpiDetail;
+			detail.setKpiID(kpi);
+		}
+		return ResponseEntity.ok(listReturn);
 	}
 
 	
