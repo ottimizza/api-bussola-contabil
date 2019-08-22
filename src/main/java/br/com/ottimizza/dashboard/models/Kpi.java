1a4fc855223eb332e92214b7e59425829039e939
DiogoMS <dmazui@gmail.com>
2019-08-22 14:11:31 -0300

    criacao de campos array nas estities kpi e kpidetail

--- src/main/java/br/com/ottimizza/dashboard/models/Kpi.java
+++ src/main/java/br/com/ottimizza/dashboard/models/Kpi.java
@@ -16,6 +16,7 @@ import javax.persistence.Index;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -102,8 +103,7 @@ public class Kpi implements Serializable {
     private String labelStringArray;
     
     @Setter
-    @ElementCollection
-    @Column(name = "label_array", nullable = true)
+    @Transient
     private List<String> labelArray;
     
 	public List<String> getLabelArray() {
