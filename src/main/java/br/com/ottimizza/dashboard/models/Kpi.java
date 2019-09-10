b0c81a58245188c0197988bbcadc6d0203e99727
DiogoMS <dmazui@gmail.com>
2019-09-10 12:00:16 -0300

    graphorder de int para Integer

--- src/main/java/br/com/ottimizza/dashboard/models/Kpi.java
+++ src/main/java/br/com/ottimizza/dashboard/models/Kpi.java
@@ -138,7 +138,7 @@ public class Kpi implements Serializable {
 	//para ordenacao dos kpis
 	@Getter
 	@Setter
-    @Column(name = "graph_order")
-	private int graphOrder;
+    @Column(name = "graph_order", nullable = true)
+	private Integer graphOrder;
 	
 }
