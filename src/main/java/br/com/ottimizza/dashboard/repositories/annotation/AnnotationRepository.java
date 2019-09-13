952403fe9dedda03944f7132bbf6ff9682c02d74
DiogoMS <dmazui@gmail.com>
2019-09-13 16:05:45 -0300

    annotation controller ajustes

--- src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
@@ -1,13 +1,16 @@
 package br.com.ottimizza.dashboard.repositories.annotation;
 
 import java.math.BigInteger;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import br.com.ottimizza.dashboard.models.Annotation;
 
+@Repository
 public interface AnnotationRepository extends JpaRepository<Annotation, BigInteger>, AnnotationRepositoryCustom {
 
 	@Query(" SELECT a FROM Annotation a WHERE id = :id ")
