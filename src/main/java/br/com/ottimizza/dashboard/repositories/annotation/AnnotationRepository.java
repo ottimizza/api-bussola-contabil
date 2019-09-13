0de5a21d8ebc2f205b51610df7a8b3130699d920
DiogoMS <dmazui@gmail.com>
2019-09-13 14:52:13 -0300

    annotation controller ajustes

--- src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
@@ -4,13 +4,16 @@ import java.math.BigInteger;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.ottimizza.dashboard.models.Annotation;
 
 @Repository
-public interface AnnotationRepository extends JpaRepository<Annotation, BigInteger>{
+public interface AnnotationRepository extends JpaRepository<Annotation, BigInteger>, AnnotationRepositoryCustom {
 
-	Optional<Annotation> findById(BigInteger id);
+	@Query(" SELECT a FROM Annotation WHERE id = :id ")
+	Annotation findAnnotationById(@Param("id") BigInteger id);
 
 }
--- src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepositoryCustom.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepositoryCustom.java
@@ -1,5 +1,10 @@
 package br.com.ottimizza.dashboard.repositories.annotation;
 
+import java.math.BigInteger;
+import java.util.Optional;
+
+import br.com.ottimizza.dashboard.models.Annotation;
+
 public interface AnnotationRepositoryCustom {
 
 }
