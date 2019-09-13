c26e1ce5894ffdc153e85ff480df6aeaa92f191e
DiogoMS <dmazui@gmail.com>
2019-09-13 17:15:38 -0300

    annotation controller ajustes

--- src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
@@ -4,9 +4,11 @@ import java.math.BigInteger;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ottimizza.dashboard.models.Annotation;
 
@@ -17,5 +19,10 @@ public interface AnnotationRepository extends JpaRepository<Annotation, BigInteg
 	Annotation findAnnotationById(@Param("id") BigInteger id);
 
 	Annotation findByKpiAlias(String kpiAlias);
+
+	@Modifying
+	@Transactional
+	@Query(" DELETE a FROM Annotation a WHERE id = :id ")
+	void deleteAnnotationById(@Param("id") BigInteger id);
 
 }
