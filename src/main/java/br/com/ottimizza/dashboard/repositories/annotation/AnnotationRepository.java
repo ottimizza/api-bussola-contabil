28646f9de75bcfda88f74310241c7ce1c5e68e72
DiogoMS <dmazui@gmail.com>
2019-09-13 16:00:21 -0300

    annotation controller ajustes

--- src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/annotation/AnnotationRepository.java
@@ -1,12 +1,10 @@
 package br.com.ottimizza.dashboard.repositories.annotation;
 
 import java.math.BigInteger;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import br.com.ottimizza.dashboard.models.Annotation;
 
