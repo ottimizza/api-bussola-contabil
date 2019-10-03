8a5164b532d3e1b2ea64ca476a21ce811bb1d3a6
DiogoMS <dmazui@gmail.com>
2019-10-03 16:23:16 -0300

    ajuste nomeclatura

---
+++ src/main/java/br/com/ottimizza/dashboard/dtos/KpiDTO.java
@@ -0,0 +1,34 @@
+package br.com.ottimizza.dashboard.dtos;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.util.List;
+
+import br.com.ottimizza.dashboard.models.KpiDetail;
+import lombok.Data;
+
+@Data
+public class KpiDTO implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private BigInteger id;
+	private BigInteger companyId;
+	private String kpiAlias;
+	private String title;
+	private String subtitle;
+	private String description;
+	private Short graphType;
+	private String columnX0Label;
+	private String label;
+	private String label2;
+	private String label3;
+	private String label4;
+	private String valueType;
+	private String labelStringArray;
+	private List<String> labelArray;
+	private List<KpiDetail> kpiDetail;
+	private String chartType;
+	private String chartOptions;
+	private Integer graphOrder;
+	private Boolean visible;
+}
