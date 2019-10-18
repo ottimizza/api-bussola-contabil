26345ea366c644736d5000f62d8af143aa5b0d14
DiogoMS <dmazui@gmail.com>
2019-10-18 17:56:43 -0300

    findVariablesByOrganizationId validacao oauth

--- src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
@@ -1,7 +1,6 @@
 package br.com.ottimizza.dashboard.repositories.organizationVariable;
 
 import java.math.BigInteger;
-import java.security.Principal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -27,24 +26,15 @@ public class OrganizationVariableRepositoryImpl implements OrganizationVariableR
 	
 	@Override
 	public List<VariableDTO> findVariablesByOrganizationId(BigInteger organizationId, UserDTO userInfo) {
-		
-		System.out.println(" --- As Queries do Diogo --- ");
-		
-		System.out.println(" UserInfo.organizationId : " + userInfo.getOrganization().getId());
-		System.out.println("          organizationId : " + organizationId);
-		
+				
 		JPAQuery<VariableDTO> query = new JPAQuery<VariableDTO>(em).from(organizationVariable)
 				.innerJoin(variable).on(
 						variable.id.eq(organizationVariable.variableId).and(organizationVariable.organizationId.eq(userInfo.getOrganization().getId())))
 				.where(organizationVariable.organizationId.eq(organizationId));
-		
-		System.out.println(" count : " + query.fetchCount());
-                
+		                
 		query.select(Projections.constructor(
 				VariableDTO.class, variable.companyId, variable.externalId, variable.name, variable.id, organizationVariable.organizationId, organizationVariable.accountingCode));
 		
-		System.out.println(" count : " + query.fetchCount());
-		System.out.println(" --- Fim Das Queries do Diogo --- ");
 		
 		return query.fetch();
 	}
