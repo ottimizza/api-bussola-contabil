7a010667f2bbf380711c376a17ad296dada22006
DiogoMS <dmazui@gmail.com>
2019-10-18 10:06:19 -0300

    findVariablesByOrganizationId

--- src/main/java/br/com/ottimizza/dashboard/repositoriesorganizationVariable/OrganizationVariableRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositoriesorganizationVariable/OrganizationVariableRepositoryImpl.java
@@ -22,7 +22,7 @@ public class OrganizationVariableRepositoryImpl implements OrganizationVariableR
 		System.out.println(">>> > "+organizationId);
 		JPAQuery<Variable> query = new JPAQuery<Variable>(em).from(variable)
 				.innerJoin(organizationVariable).on(organizationVariable.accountingCode.eq(variable.accountingCode))
-				.where(organizationVariable.organizationId.eq(variable.id));
+				.where(organizationVariable.organizationId.eq(organizationId));
 		return query.fetch();
 	}
 }
