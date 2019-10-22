eb42da45c8aa3f236d5851a864dc8d501cfbbc35
DiogoMS <dmazui@gmail.com>
2019-10-22 14:22:41 -0300

    findVariablesByCompanyId

--- src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepository.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepository.java
@@ -1,19 +1,14 @@
 package br.com.ottimizza.dashboard.repositories.organizationVariable;
 
 import java.math.BigInteger;
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import br.com.ottimizza.dashboard.dtos.UserDTO;
-import br.com.ottimizza.dashboard.dtos.VariableDTO;
 import br.com.ottimizza.dashboard.models.OrganizationVariable;
 
-@Repository
+
 public interface OrganizationVariableRepository extends JpaRepository<OrganizationVariable, BigInteger>, OrganizationVariableRepositoryCustom {
 
-//	List<VariableDTO> findVariablesByCompanyId(BigInteger companyId, UserDTO userInfo);
 
 
 }
