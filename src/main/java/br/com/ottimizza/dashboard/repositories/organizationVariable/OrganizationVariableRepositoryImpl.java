663e8443a5cd4e195c2ba3575e99c00ffae13422
DiogoMS <dmazui@gmail.com>
2019-10-23 09:57:02 -0300

    ajust nomes e implementacoes

--- src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
@@ -55,8 +55,8 @@ public class OrganizationVariableRepositoryImpl implements OrganizationVariableR
 	public List<VariableDTO> findMissingByOrganizationId(BigInteger companyId, UserDTO userInfo) {
 		JPAQuery<VariableDTO> query = new JPAQuery<VariableDTO>(em).from(variable)
 				.leftJoin(organizationVariable).on(
-						organizationVariable.variableId.eq(variable.id).and(variable.accountingId.eq(userInfo.getOrganization().getId())))
-				.where(organizationVariable.id.isNull());
+						organizationVariable.variableId.eq(variable.id).and(organizationVariable.organizationId.eq(companyId)))
+				.where(organizationVariable.id.isNull().and(variable.accountingId.eq(userInfo.getOrganization().getId())));
 				
 		query.select(Projections.constructor(
 				VariableDTO.class, variable.accountingId, variable.externalId, variable.name, variable.id, organizationVariable.organizationId, variable.accountingCode));
