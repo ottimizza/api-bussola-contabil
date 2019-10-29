96caf89925a1c2944c6a114c3fb7cb65cec34f6e
DiogoMS <dmazui@gmail.com>
2019-10-29 11:00:31 -0300

    incluida 'sector' in company

--- src/main/java/br/com/ottimizza/dashboard/dtos/AnnotationDTO.java
+++ src/main/java/br/com/ottimizza/dashboard/dtos/AnnotationDTO.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 
 import br.com.ottimizza.dashboard.models.Annotation;
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
