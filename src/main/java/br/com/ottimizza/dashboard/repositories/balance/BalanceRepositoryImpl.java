0d49518be1b8e0116aadba983da4d0e125ca59d5
DiogoMS <dmazui@gmail.com>
2019-11-01 16:20:07 -0300

    find by tudo

--- src/main/java/br/com/ottimizza/dashboard/repositories/balance/BalanceRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/balance/BalanceRepositoryImpl.java
@@ -12,6 +12,7 @@ import br.com.ottimizza.dashboard.dtos.BalanceDTO;
 import br.com.ottimizza.dashboard.models.Balance;
 import br.com.ottimizza.dashboard.models.QBalance;
 import br.com.ottimizza.dashboard.models.QCompany;
+import br.com.ottimizza.dashboard.utils.StringUtil;
 
 public class BalanceRepositoryImpl implements BalanceRepositoryCustom{
 
@@ -36,7 +37,7 @@ public class BalanceRepositoryImpl implements BalanceRepositoryCustom{
 		JPAQuery<Balance> query = new JPAQuery<Balance>(em).from(balance)
                 .innerJoin(company).on(company.id.eq(balance.companyId));
 		
-		if(balanceDTO.getCnpj() != null)		query.where(company.cnpj.eq(balanceDTO.getCnpj()));
+		if(balanceDTO.getCnpj() != null)		query.where(company.cnpj.eq(StringUtil.formatCnpj(balanceDTO.getCnpj())));
 		if(balanceDTO.getDateBalance() != null)	query.where(balance.dateBalance.eq(balanceDTO.getDateBalance()));
 		if(balanceDTO.getSyntheticId() != null) query.where(balance.syntheticId.eq(balanceDTO.getSyntheticId()));
 		if(balanceDTO.getAnalyticId() != null)	query.where(balance.analyticId.eq(balanceDTO.getAnalyticId()));
