a074045dc6f09db1906796e3ea2d0153ff07e816
DiogoMS <dmazui@gmail.com>
2019-12-11 16:58:26 -0300

    account id

--- src/main/java/br/com/ottimizza/dashboard/services/CompanyService.java
+++ src/main/java/br/com/ottimizza/dashboard/services/CompanyService.java
@@ -138,7 +138,7 @@ public class CompanyService {
 	
 	public List<OrganizationDTO> findOrganizationInfo(String authorization, OrganizationDTO filter) throws Exception {
 		String cnpj = StringUtils.leftPad(filter.getCnpj().replaceAll("\\D", ""), 14, "0");
-		HttpEntity<GenericResponse<List<OrganizationDTO>>> teste = oauthCliente.getOrganizationInfo(authorization, cnpj);
+		HttpEntity<GenericResponse<OrganizationDTO>> teste = oauthCliente.getOrganizationInfo(authorization, cnpj);
 		System.out.println("TOKEN:" + authorization);
 		System.out.println("CNPJ:" + cnpj);
 		
