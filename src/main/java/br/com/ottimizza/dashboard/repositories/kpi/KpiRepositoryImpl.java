a8d0ae067a4c5b99a296fd338168e7eb19654d12
Diogo Mazui <dmazui@gmail.com>
2020-02-18 09:56:02 -0300

    removido filtro por kpialias 12 e 7

--- src/main/java/br/com/ottimizza/dashboard/repositories/kpi/KpiRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/kpi/KpiRepositoryImpl.java
@@ -64,8 +64,9 @@ public class KpiRepositoryImpl implements KpiRepositoryCustom {
 				.and(description.kpiAlias.eq(kpi.kpiAlias)));
 		query.where(kpi.visible.isTrue()
 				.and(description.visible.isTrue())
-				.and(kpi.kpiAlias.notLike("07"))
-				.and(kpi.kpiAlias.notLike("12")));
+//				.and(kpi.kpiAlias.notLike("07"))
+//				.and(kpi.kpiAlias.notLike("12"))
+				);
 
 		if (filter.getCnpj() != null) {
 			query.where(company.cnpj.eq(StringUtil.formatCnpj(filter.getCnpj())));
