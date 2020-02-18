7ca2ebc925d5eaa8ae960c9a1f4f76bfc6b55799
Diogo Mazui <dmazui@gmail.com>
2020-02-18 18:05:04 -0300

    mudanca no save description

--- src/main/java/br/com/ottimizza/dashboard/services/DescriptionService.java
+++ src/main/java/br/com/ottimizza/dashboard/services/DescriptionService.java
@@ -78,15 +78,17 @@ public class DescriptionService {
 				}
 			}
 			List<ScriptType> scripts = scriptTypeRepository.findAll(new ScriptTypeDTO(null, null, descriptionDTO.getScriptDescription()));
-
-			if(descriptionDTO.getScriptDescription() != null) {
-				if(scripts.size() > 0)		 descriptionDTO.setScriptId(scripts.get(0).getId());
-				else if(scripts.size() == 0) descriptionDTO.setScriptId(scriptTypeRepository.save(new ScriptType(null, descriptionDTO.getAccountingId(), descriptionDTO.getScriptDescription())).getId());
-			} else {
-				scripts = scriptTypeRepository.findAll(new ScriptTypeDTO(null, null, "PADRAO"));
-				if(scripts.size() > 0)		 descriptionDTO.setScriptId(scripts.get(0).getId());
-				else if(scripts.size() == 0) descriptionDTO.setScriptId(scriptTypeRepository.save(new ScriptType(null, descriptionDTO.getAccountingId(), "PADRAO")).getId());
+			if(scripts.size() > 0 && descriptionDTO.getScriptDescription() != null) {
+				descriptionDTO.setScriptId(scripts.get(0).getId());
 			}
+//			if(descriptionDTO.getScriptDescription() != null) {
+//				if(scripts.size() > 0)		 descriptionDTO.setScriptId(scripts.get(0).getId());
+//				else if(scripts.size() == 0) descriptionDTO.setScriptId(scriptTypeRepository.save(new ScriptType(null, descriptionDTO.getAccountingId(), descriptionDTO.getScriptDescription())).getId());
+//			} else {
+//				scripts = scriptTypeRepository.findAll(new ScriptTypeDTO(null, null, "PADRAO"));
+//				if(scripts.size() > 0)		 descriptionDTO.setScriptId(scripts.get(0).getId());
+//				else if(scripts.size() == 0) descriptionDTO.setScriptId(scriptTypeRepository.save(new ScriptType(null, descriptionDTO.getAccountingId(), "PADRAO")).getId());
+//			}
 		}
 
 		Description description2 = new Description();
