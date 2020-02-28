7ceaa5b22c1094ddc9be3d1e019e3ed99f7df8cb
Diogo Mazui <dmazui@gmail.com>
2020-02-28 09:59:45 -0300

    ajust

--- src/main/java/br/com/ottimizza/dashboard/repositories/description/DescriptionRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/description/DescriptionRepositoryImpl.java
@@ -16,6 +16,7 @@ import com.querydsl.jpa.impl.JPAQuery;
 import br.com.ottimizza.dashboard.domain.dtos.DescriptionDTO;
 import br.com.ottimizza.dashboard.models.Company;
 import br.com.ottimizza.dashboard.models.Description;
+import br.com.ottimizza.dashboard.models.QCompany;
 import br.com.ottimizza.dashboard.models.QDescription;
 import br.com.ottimizza.dashboard.services.CompanyService;
 import br.com.ottimizza.dashboard.utils.StringUtil;
@@ -31,6 +32,8 @@ public class DescriptionRepositoryImpl implements DescriptionRepositoryCustom {
 	
 	private QDescription description = QDescription.description1;
 
+	private QCompany company = QCompany.company;
+			
 	@Override
 	public List<Description> findAll(DescriptionDTO descriptionDto) {
 
@@ -47,10 +50,13 @@ public class DescriptionRepositoryImpl implements DescriptionRepositoryCustom {
 	@Override
 	public Page<Description> findByAccountingIdScriptType(DescriptionDTO descriptionDTO, Pageable pageable) {
 
-		JPAQuery<Description> query = new JPAQuery<Description>(em);
-		query.from(description);
-
-		Company company = new Company();
+		JPAQuery<Description> query = new JPAQuery<Description>(em).from(description);
+		
+		if (descriptionDTO.getCnpj() != null) {
+			query.innerJoin(company).on(company.cnpj.eq(description.cnpj));
+		}
+		
+		Company company2 = new Company();
 		long totalDescriptions = 0;
 
 		if (descriptionDTO.getId() != null)				query.where(description.id.eq(descriptionDTO.getId()));
@@ -61,15 +67,19 @@ public class DescriptionRepositoryImpl implements DescriptionRepositoryCustom {
 		if (descriptionDTO.getTitle() != null) 			query.where(description.title.eq(descriptionDTO.getTitle()));
 		if (descriptionDTO.getGraphOrder() != null) 	query.where(description.graphOrder.eq(descriptionDTO.getGraphOrder()));
 		if (descriptionDTO.getChartType() != null) 		query.where(description.chartType.eq(descriptionDTO.getChartType()));
+		
 		if (descriptionDTO.getCnpj() != null) {
+					
 			try {
-				company = service.findByCnpj(StringUtil.formatCnpj(descriptionDTO.getCnpj()));
-				if(company != null) {
-					query.where(description.scriptId.eq(company.getScriptId()));
+				company2 = service.findByCnpj(StringUtil.formatCnpj(descriptionDTO.getCnpj()));
+				if(company2 != null) {
+					query.where(description.scriptId.eq(company2.getScriptId()));
 				}
+				
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+			query.where(company.cnpj.eq(StringUtil.formatCnpj(descriptionDTO.getCnpj())));
 		}
 			
 			//query.where(description.cnpj.eq(StringUtil.formatCnpj(descriptionDTO.getCnpj())));
@@ -77,7 +87,7 @@ public class DescriptionRepositoryImpl implements DescriptionRepositoryCustom {
 
 		totalDescriptions = query.fetchCount();
 		query.limit(pageable.getPageSize());
-		query.offset(pageable.getPageSize() *pageable.getPageNumber());
+		query.offset(pageable.getPageSize() * pageable.getPageNumber());
 
 		return new PageImpl<Description>(query.orderBy(description.graphOrder.asc()).fetch(), pageable, totalDescriptions);
 	}	
