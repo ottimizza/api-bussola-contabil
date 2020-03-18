b8c5c34703990649263613ba4d8fc57a6a49e4f2
Diogo Mazui <dmazui@gmail.com>
2020-03-18 17:57:08 -0300

    removendo cnpj

--- src/main/java/br/com/ottimizza/dashboard/services/OrganizationVariableService.java
+++ src/main/java/br/com/ottimizza/dashboard/services/OrganizationVariableService.java
@@ -122,6 +122,7 @@ public class OrganizationVariableService {
 			Company cia = companyRepository.findByCnpj(StringUtil.formatCnpj(filter.getCnpj()));
 			filter.setScriptId(cia.getScriptId());
 			filter.setAccountingId(cia.getAccountingId());
+			filter.setCnpj(null);
 		} catch (Exception e) {  }
 
 		return repository.findMissingByCompanyId(filter, userInfo);
