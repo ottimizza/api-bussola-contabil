82e5f19f44c02d592d5b9281f3e0ad3025116c0e
Diogo Mazui <dmazui@gmail.com>
2020-03-19 08:21:31 -0300

    mudanca no left join

--- src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
@@ -65,7 +65,7 @@ public class OrganizationVariableRepositoryImpl implements OrganizationVariableR
 				 .and(company.cnpj.eq(StringUtil.formatCnpj(filter.getCnpj())))
 				 .and(organizationVariable.scriptId.eq(variable.scriptId)));
 		}
-		System.out.println(">>> c> "+query.fetchCount());
+//		System.out.println(">>> c> "+query.fetchCount());
 //		query.where(variable.accountingId.eq(filter.getAccountingId()));
 		System.out.println(">>> d> "+query.fetchCount());
 		query.where(organizationVariable.id.isNull());
