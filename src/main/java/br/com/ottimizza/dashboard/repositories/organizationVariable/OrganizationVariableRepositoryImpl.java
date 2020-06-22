7f42b2dfeee376fb51f4bbf9fa0bea7919a3d04d
Carlos Eduardo da Costa Xavier <54960151+carlosxis@users.noreply.github.com>
2020-06-22 11:20:30 -0300

    Update OrganizationVariableRepositoryImpl.java

--- src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
+++ src/main/java/br/com/ottimizza/dashboard/repositories/organizationVariable/OrganizationVariableRepositoryImpl.java
@@ -53,14 +53,10 @@ public class OrganizationVariableRepositoryImpl implements OrganizationVariableR
 
  		JPAQuery<VariableDTO> query = new JPAQuery<VariableDTO>(em).from(variable);
 		
-		if(filter.getScriptId() != null) {
-			query.innerJoin(company).on(company.accountingId.eq(variable.accountingId)
-			 .and(company.scriptId.eq(variable.scriptId))
-			 .and(company.scriptId.eq(filter.getScriptId())));
-		} else {
-			query.innerJoin(company).on(company.accountingId.eq(variable.accountingId)
-			 .and(company.scriptId.eq(variable.scriptId))); 
-		}
+		query.innerJoin(company).on(company.accountingId.eq(variable.accountingId)
+		 .and(company.scriptId.eq(variable.scriptId))
+		 .and(company.scriptId.eq(filter.getScriptId())));
+		 
 
 		if(filter.getCompanyId() != null) {
 			query.leftJoin(organizationVariable).on(organizationVariable.variableId.eq(variable.id)
